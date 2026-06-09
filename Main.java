428e878ef4c3be50d321d0ed4b68e7eb013898ce
AndersonDuranH <anderson_duran@ucne.edu.do>
2026-06-08 20:29:40 -0400

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-
-        Estudiante estudiante = new Estudiante("", 0, 0.0);
-
-        Estudiante.Menu();
-    }
-}
